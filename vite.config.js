60dac31aa574c17c710dd727565dfdc6d4256c5d
Nikita <bazilik2452@gmail.com>
2023-07-04 17:51:24 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fixed-card-users/",
+  base: "/fixed-cerd-users/",
   plugins: [react()],
 });
